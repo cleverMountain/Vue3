964ee7d7711f8184131044ecd2b1a30b60968cc7
huangjunlin <9579711+huangjunlin511024@user.noreply.gitee.com>
2023-10-08 22:33:06 +0800

    proxy

--- 1.js
+++ 1.js
@@ -1,33 +1,37 @@
-## ref
-1. ref是一个函数，传入一个值返回一个响应式的对象
-```js
+// @ts-nocheck
+const isObject = (val) => val !== null && typeof val === "object";
+const toReactive = (value) => isObject(value) ? reactive(value) : value;
+function isRef(r) {
+  return !!(r && r.__v_isRef === true);
+}
 function ref(value) {
   return createRef(value, false);
 }
 function createRef(rawValue, shallow) {
-  // 已经是响应式对象则返回该响应式对象
   if (isRef(rawValue)) {
     return rawValue;
   }
-  // 响应式对象
   let res = new RefImpl(rawValue, shallow);
+  console.log(res)
   return res
+}
+function toRaw(observed) {
+  const raw = observed && observed["__v_raw"];
+  return raw ? toRaw(raw) : observed;
 }
 class RefImpl {
   constructor(value, __v_isShallow) {
     this.__v_isShallow = __v_isShallow;
     this.dep = void 0;
     this.__v_isRef = true;
+ 
     this._rawValue = __v_isShallow ? value : toRaw(value);
-    // 使用_value代理value
     this._value = __v_isShallow ? value : toReactive(value);
   }
-  // .value时调用get返回_vakue
   get value() {
-    trackRefValue(this);
+
     return this._value;
   }
-  // .value = ?时
   set value(newVal) {
     const useDirectValue = this.__v_isShallow || isShallow(newVal) || isReadonly(newVal);
     newVal = useDirectValue ? newVal : toRaw(newVal);
@@ -38,4 +42,5 @@ class RefImpl {
     }
   }
 }
-```
+const message = ref('hello')
+console.log(message)
